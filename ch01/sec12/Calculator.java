105c3c374366209828ec2abc7844c7576c2a1e3d
sunju1 <ciwrssn10@naver.com>
2025-07-18 23:47:26 +0900

    변수 선언 및 연산

---
+++ ch01/sec12/Calculator.java
@@ -0,0 +1,13 @@
+package ch01.sec12;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		
+		System.out.println(result);
+	}
+}
+
+/* 실행 코드 세미콜론으로 구분 */
